69a61acd49038581580b5fe6cf7e8d4bde7ad97f
SebixR <sebarog@gmail.com>
2024-05-17 17:56:37 +0200

    Editing account information finished. Email can never be changed

--- src/Components/AccountPage/AccountPage.jsx
+++ src/Components/AccountPage/AccountPage.jsx
@@ -6,14 +6,16 @@ import {faCheck, faPencilSquare, faTrash} from "@fortawesome/free-solid-svg-icon
 import Item from "../Item/Item";
 import axios from "../../config/axios";
 import {useAuth} from "../../provider/AuthProvider";
+import ErrorNotification from "../ErrorNotification/ErrorNotification";
+import SuccessNotification from "../SuccessNotification/SuccessNotification";
 
 const AccountPage = () => {
 
     const [userId, setUserId] = useState(null)
     const [inputValues, setInputValues] = useState({
-        firstNameInput: '',
-        lastNameInput: '',
-        emailInput: ''
+        firstName: '',
+        lastName: '',
+        email: ''
     })
     const { token } = useAuth();
     const [items, setItems] = useState([]);
@@ -28,9 +30,9 @@ const AccountPage = () => {
                 const userDataResponse = response.data;
                 setUserId(userDataResponse.id);
                 setInputValues({
-                    firstNameInput: userDataResponse.firstName,
-                    lastNameInput: userDataResponse.lastName,
-                    emailInput: userDataResponse.email
+                    firstName: userDataResponse.firstName,
+                    lastName: userDataResponse.lastName,
+                    email: userDataResponse.email
                 })
             } catch (error) {
                 console.log(error);
@@ -55,6 +57,53 @@ const AccountPage = () => {
         }
 
     }, [userId]);
+
+
+    const [errors, setErrors] = useState([]);
+    const validateForm = () => {
+        let errors = {};
+        let isValid = true;
+
+        if (!inputValues.firstName.trim()) errors.firstName = 'First name is required!';
+        if (!/^[a-zA-Z]+$/.test(inputValues.firstName)) errors.firstName = 'First name can only contain letters';
+        if (!inputValues.lastName.trim()) errors.lastName = 'Last name is required!';
+        if (!/^[a-zA-Z]+$/.test(inputValues.lastName)) errors.lastName = 'Last name can only contain letters';
+        if (!inputValues.email.trim()) errors.email = 'Email is required!';
+        if (!/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/.test(inputValues.email)) {
+            errors.email = 'Incorrect email!';
+        }
+
+        if (Object.keys(errors).length !== 0) isValid = false;
+
+        setErrors(errors);
+        return isValid;
+    }
+    const [success, setSuccess] = useState(false);
+    const handleSubmitUserInfo = async (e) => {
+        e.preventDefault();
+
+        let isValid = validateForm();
+        if (!isValid) return;
+
+        const firstName = inputValues.firstName;
+        const lastName = inputValues.lastName;
+        const email = inputValues.email;
+        const formData = { firstName, lastName, email};
+        try {
+            const response = await axios.post('/updateAppUser', formData);
+            console.log(response.data);
+            showSuccessNotification();
+            toggleReadOnly();
+        } catch (error) {
+
+        }
+    }
+    const showSuccessNotification = () => {
+        setSuccess(true);
+        setTimeout(() => {
+            setSuccess(false);
+        }, 3000);
+    };
 
 
     const [isReadOnly, setIsReadOnly] = useState(true);
@@ -88,21 +137,27 @@ const AccountPage = () => {
 
             <TopNav/>
 
+            {Object.keys(errors).map((errorKey) => (
+                <ErrorNotification key={errorKey} text={errors[errorKey]} />
+            ))}
+
+            {success && <SuccessNotification text="Successfully changed user information." />}
+
             <div className="account-main-content-wrap">
 
-                <div className="account-info-pane">
+                <form onSubmit={handleSubmitUserInfo} className="account-info-pane">
                     <div className="account-info-content">
                         <div className="single-info-wrap">
                             <label>First Name</label>
-                            <input disabled={isReadOnly} type="text" name="firstNameInput" onChange={handleInputChange} value={inputValues.firstNameInput} readOnly={isReadOnly}/>
+                            <input disabled={isReadOnly} type="text" name="firstName" onChange={handleInputChange} value={inputValues.firstName} readOnly={isReadOnly}/>
                         </div>
                         <div className="single-info-wrap">
                             <label>Last Name</label>
-                            <input disabled={isReadOnly} type="text" name="lastNameInput" onChange={handleInputChange} value={inputValues.lastNameInput} readOnly={isReadOnly}/>
+                            <input disabled={isReadOnly} type="text" name="lastName" onChange={handleInputChange} value={inputValues.lastName} readOnly={isReadOnly}/>
                         </div>
                         <div className="single-info-wrap">
                             <label>Email</label>
-                            <input disabled={isReadOnly} type="text" name="emailInput" onChange={handleInputChange} value={inputValues.emailInput} readOnly={isReadOnly}/>
+                            <input disabled={true} type="text" name="email" onChange={handleInputChange} value={inputValues.email} readOnly={isReadOnly}/>
                         </div>
                     </div>
 
@@ -116,7 +171,7 @@ const AccountPage = () => {
                             <FontAwesomeIcon icon={faCheck} className="icon"/>
                         </button>
                     </div>
-                </div>
+                </form>
 
                 <button type="button">Change Password</button>
 
