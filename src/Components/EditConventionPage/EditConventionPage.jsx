843739956bb743fd17197c7f76fb16a9c37c9dbd
SebixR <sebarog@gmail.com>
2024-05-27 17:07:11 +0200

    Unnecessary import

--- src/Components/EditConventionPage/EditConventionPage.jsx
+++ src/Components/EditConventionPage/EditConventionPage.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react"
-import TopNav from "../TopNav/TopNav";
 import AddConventionPage from "../AddConventionPage/AddConventionPage";
 import {useParams} from "react-router-dom";
 import axios from "../../config/axios";
